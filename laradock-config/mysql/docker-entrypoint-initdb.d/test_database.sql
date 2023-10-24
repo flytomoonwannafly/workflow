7bccf865f5b893257b1812af1a632b3beb4faa4f
Володимир Іваніцький <volodimirivanitskyi@MacBook-Pro-Volodimir.local>
2023-10-24 14:22:55 +0300

    check simple db

--- laradock-config/mysql/docker-entrypoint-initdb.d/test_database.sql
+++ laradock-config/mysql/docker-entrypoint-initdb.d/test_database.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Хост: mysql
--- Час створення: Жов 23 2023 р., 11:42
--- Версія сервера: 8.1.0
--- Версія PHP: 8.2.11
+-- Host: mysql
+-- Generation Time: Sep 18, 2023 at 05:11 PM
+-- Server version: 5.6.51
+-- PHP Version: 8.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,5 +18,5 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- База даних: `SkillUp`
+-- Database: `truelearn_db`
 --
