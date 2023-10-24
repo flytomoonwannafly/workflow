97cb0cf8821db858b6924f2ca4cd492d0afe468c
Володимир Іваніцький <volodimirivanitskyi@MacBook-Pro-Volodimir.local>
2023-10-24 17:40:34 +0300

    add db for building

---
+++ laradock-config/mysql/docker-entrypoint-initdb.d/test_database.sql
@@ -0,0 +1,22 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: mysql
+-- Generation Time: Sep 18, 2023 at 05:11 PM
+-- Server version: 5.6.51
+-- PHP Version: 8.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `truelearn_db`
+--
