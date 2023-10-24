f8bcb6ad2d0cab65376aa33516730714a1b8eb1d
Володимир Іваніцький <volodimirivanitskyi@MacBook-Pro-Volodimir.local>
2023-10-24 12:11:33 +0300

    chandge dir db

---
+++ laradock-config/mysql/docker-entrypoint-initdb.d/SkillUp_table_Notes.sql
@@ -0,0 +1,15 @@
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблиці `Notes`
+--
+
+CREATE TABLE `Notes` (
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `content` text COLLATE utf8mb4_unicode_ci,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
